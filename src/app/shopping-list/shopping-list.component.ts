ae5f11494ea85d8baf1ffecff90d4e83f8d788e7
Dani Matuko <matuko305@gmail.com>
2023-03-06 00:48:41 +0200

    add Ingredient model and shoppong-edit html

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ingredient } from 'src/models/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomeatos', 10),
+  ];
 }
